83ae4a0037771ba516c0b8c33de61365537be8b7
Alexander Stepanov <74261544+alxstx@users.noreply.github.com>
2021-02-15 18:58:25 +0100

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -8,19 +8,19 @@ n = random.choice(lists)
 b = 100
 a = 100
 for item in lists:
-    if int(b) == n:
+    if b == n:
         print('YOU WON', n, 'ONLY IN', item - 2, 'TRIES')
         break
     if a < b:
         raise ArithmeticError('There is an Error', a, b, n)
-    if int(item) == 1:
+    if item == 1:
         b = a / 2
-    elif int(b) > n:
+    elif b > n:
         r = a - b
         a = b
         b = b - r / 2
         print(b)
-    elif int(b) < n:
+    elif b < n:
         r = a - b
         c = r / 2
         b = b + c
